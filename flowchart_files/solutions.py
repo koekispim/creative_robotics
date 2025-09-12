cb9952231f8369176893e75d89b55d291266e6f1
hannahle <hannahlee.lor@gmail.com>
2025-09-12 10:04:21 +0200

    hannah-exercises

--- flowchart_files/solutions.py
+++ flowchart_files/solutions.py
@@ -2,4 +2,24 @@
 each student does one on their computer and commit/push to the repository"""
 
 
-print("hello koen")
+# ------ Flowchart Exercise 2 -----
+def flowchart2(n):
+    n = n+2
+    print(n)
+
+n = int(input("Enter a number: "))
+flowchart2(n)
+
+
+
+# ------ Flowchart Exercise 4 -----
+def flowchart4(i):
+    if i > 0:
+        print("Greater than 0")
+    elif i < 0:
+        print("Less than 0")
+    else:
+        print("Done")
+
+i = int(input("Enter a number: "))
+flowchart4(i)
