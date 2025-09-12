bc8deee002164d853c2dad0ae387af6cb7b58b29
4SAMI <catsanddogsandrabbits.graduate019@aleeas.com>
2025-09-12 10:04:18 +0200

    adding my solutions

--- flowchart_files/solutions.py
+++ flowchart_files/solutions.py
@@ -1,6 +1,18 @@
 """Enter here your solutions to the flowchart exercises. 
 each student does one on their computer and commit/push to the repository"""
 
+# ---------- exercise 1 ---------- #
+def exercise1(output):
+    print(output)
 
-# test 
-print('Hello, Hannah!')
+exercise1('hello')
+
+
+# ---------- exercise 3 ---------- #
+def exercise3(input):
+    if input > 0:
+        print('Greater than 0')
+    else:
+        pass
+
+exercise3(eval(input('Enter a number')))
